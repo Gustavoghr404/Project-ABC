d6651fff0f9ada8d79808afd10cb6ef995f76ef5
Gustavoghr404 <163424638+Gustavoghr404@users.noreply.github.com>
2024-03-16 19:21:01 -0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,7 @@
+function changeColor(button) {
+    button.style.backgroundColor = "#28a745"; // muda a cor para verde quando o botão é pressionado
+}
+
+function revertColor(button) {
+    button.style.backgroundColor = "#007bff"; // volta a cor para azul quando o botão é solto
+}
